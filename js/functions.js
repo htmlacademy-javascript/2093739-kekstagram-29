f43e86a1902a727f8d48f4bf146a4e5dc8dc6cec
nibelung86 <kitnibelung@gmail.com>
2023-07-27 16:48:44 +0300

    func stringLength + script plug

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+// Функция для проверки длины строки
+
+const isLessOrEqual = (string, stringLength) => string.length <= stringLength;
+isLessOrEqual('провод', 10); //true
+isLessOrEqual('проводник', 6); //false
